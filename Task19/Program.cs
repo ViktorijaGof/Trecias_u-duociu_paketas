3f9183deb7984428d00369df1811afc4a1fd31f2
Viktorija Goferienė <v.goferiene@gmail.com>
2023-04-13 15:53:38 +0300

    Add 12-20 task

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -10,6 +10,29 @@ namespace Task19
     {
         static void Main(string[] args)
         {
+            //Parašyti For ciklą, kuris suskaičiuotų, kiek skaičių intervale [0 – 1000) dalinasi iš 16.
+
+            int dalijasiIs16 = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 16 == 0)
+                {
+                 dalijasiIs16++;
+                }
+            }
+            Console.WriteLine("Skaičių intervale [0 - 1000) dalijasi iš 16: " + dalijasiIs16);
+            Console.WriteLine();
+
+            //Liepkite vartotojui įvesti teigiamą skaičių.While ciklo viduje skaičių kelkite vis didesniu laipsniu ir jį išveskite į ekraną, kol skaičius netaps didesnis už 1500.
+        
+            Console.WriteLine("Įveskite teigiamą skaičių:");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+
+            while (skaicius <= 1500) 
+            {
+                Console.WriteLine(skaicius);
+                skaicius *= 2;
+            }
         }
     }
 }
