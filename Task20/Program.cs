0a6cf6e58625eb0a464bb12e3756443a8c4a9827
Viktorija Goferienė <v.goferiene@gmail.com>
2023-04-16 23:03:27 +0300

    Add new Tasks to 12-20 Tasks

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Task20
 {
@@ -10,6 +7,19 @@ namespace Task20
     {
         static void Main(string[] args)
         {
+            //Įveskite iš konsolės 2 skaičius (stataus trikampio statinių ilgius).
+            //Suraskite įžambinę ir ją išveskite į ekraną.
+            
+            Console.WriteLine("Įveskite a statinio ilgį:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Įveskite b statinio ilgį:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b)); 
+            Console.WriteLine("Įžambinė: " + c);
+
         }
     }
 }
+ 
